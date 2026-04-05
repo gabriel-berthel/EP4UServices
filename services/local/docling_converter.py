a6d9eac504488ad88781d768a13e50680c6342da
Gabriel <gabriel.berthel@etu.univ-tours.fr>
2026-04-04 20:26:52 -0400

    patch??

--- services/local/docling_converter.py
+++ services/local/docling_converter.py
@@ -1,29 +1,26 @@
 from docling.datamodel.base_models import InputFormat
 from docling.datamodel.pipeline_options import PdfPipelineOptions
-from docling.document_converter import DocumentConverter, PdfFormatOption 
+from docling.document_converter import DocumentConverter, PdfFormatOption
 from docling_surya import SuryaOcrOptions
 from docling.datamodel.accelerator_options import AcceleratorDevice, AcceleratorOptions
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
-
 from services.interfaces import ParseInterface
 
+from transformers.models.auto.configuration_auto import AutoConfig
 
-# Surya OCR pad_token_id patch
-# Cuz newer versions of Docling > Transformers v5. Surya still on v4
-tokenizer = AutoTokenizer.from_pretrained("vikp/surya_rec")
-model = AutoModelForSeq2SeqLM.from_pretrained("vikp/surya_rec")
+# Manually add the missing attribute to the Surya config class if it's missing
+from transformers import AutoConfig
 
-if getattr(model.config, "pad_token_id", None) is None:
-    if tokenizer.pad_token_id is not None:
-        model.config.pad_token_id = tokenizer.pad_token_id
-    else:
-        tokenizer.pad_token = tokenizer.eos_token
-        model.config.pad_token_id = tokenizer.pad_token_id
+# Pre-load and patch the config in memory
+config = AutoConfig.from_pretrained("vikp/order_hr")
+if not hasattr(config, "pad_token_id"):
+    config.pad_token_id = 1 
+
+
 
 class DoclingConverter(ParseInterface):
     def __init__(self):
         pipeline_options = PdfPipelineOptions(
-        do_ocr=True,
+            do_ocr=True,
             ocr_model="suryaocr",
             allow_external_plugins=True,
             ocr_options=SuryaOcrOptions(lang=["en"], force_full_page_ocr=True, use_gpu=True),
@@ -43,9 +40,11 @@ class DoclingConverter(ParseInterface):
         
         self.converter = DocumentConverter(
             format_options={
-                InputFormat.PDF: PdfFormatOption(pipeline_options=pipeline_options)
+                InputFormat.PDF: PdfFormatOption(pipeline_options=pipeline_options),
             }
         )
+    
+        self.converter._get_pipeline(InputFormat.PDF)
         
     def run(self, path):
         return self.converter.convert(path)
